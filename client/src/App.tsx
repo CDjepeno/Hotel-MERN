b677e925c0751dfa2e80933acb2f0acddfc9df0c
CDjepeno <djepeno.christopher@gmail.com>
2021-03-27 18:35:35 +0100

    Install react

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { disconnect } from 'node:process';
+
+const App = () => {
+  return (
+    <div>
+      <h1>home</h1>
+    </div>
+  );
+}
+
+export default App;
