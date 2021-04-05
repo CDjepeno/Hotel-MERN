d5a94d87a2d4e58ba131ebfba1f86a22d340bce0
CDjepeno <djepeno.christopher@gmail.com>
2021-04-05 04:16:07 +0200

    Fix routes and delete docs config

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,7 @@
 import express from 'express'
-import {config} from '../Docs-API/docs-config.js'
-import {server} from '../Docs-API/docs-config.js'
 import {getRooms,deleteRooms,getOneRoom,updateRoom,addRoom} from '../controllers/roomControllers.js'
 import {register, login, logout} from '../controllers/userController.js'
-// import { auth } from '../client/auth/auth.js'
+import { auth } from '../auth/auth.js'
 // Path with ES module
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
@@ -14,32 +12,30 @@ const __dirname = dirname(__filename)
 const router = express.Router()
 const app = express()
 
-/**
- * Documentation API
- */
-router.use('/api-docs', server, config)
-
-
 router.get('/', (_,res) => {
     res.send('home page')
 })
 
-router.get('/api/rooms', getRooms)
+router.get('/api/rooms',auth, getRooms)
 
-router.put('/api/rooms/:id', updateRoom)
+router.put('/api/rooms/:id',auth, updateRoom)
 
-router.post('/api/rooms', addRoom)
+router.post('/api/rooms',auth, addRoom)
 
 router.post('/api/register', register)
 
 router.post('/api/login', login)
 
-router.get('/api/rooms/:id', getOneRoom)
+router.get('/api/rooms/:id',auth, getOneRoom)
 
-router.get('/api/logout', logout)
+router.get('/api/logout',auth, logout)
 
-router.delete('/api/rooms/:id', deleteRooms)
+router.delete('/api/rooms/:id',auth, deleteRooms)
 
+
+/**
+ * In production
+ */
 //  router.get('/*', (_,res) => {
 //     res.sendFile(path.join(__dirname,'../client/build/index.html'))
 //  })
